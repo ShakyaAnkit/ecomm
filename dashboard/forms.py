43526dac97837d3b37e8ebc82b533dda9398ba74
pra5151 <spranish45@gmail.com>
2021-05-22 21:15:14 +0545

    login and logout view and added form

---
+++ dashboard/forms.py
@@ -0,0 +1,69 @@
+from django.contrib.auth.models import Group, User
+from django import forms
+from django.utils.html import mark_safe
+
+from .mixins import FormControlMixin 
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder':  'Username'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder':  'Password'}))
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = User.objects.filter(username=username, is_active=True).first()
+        if user == None or not user.check_password(password):
+            raise forms.ValidationError("Incorrect username or password")
+        return self.cleaned_data
+
+class ChangePasswordForm(forms.Form):
+    current_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder':  'Current Password'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder':  'Password'}))
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder':  'Confirm Password'}))
+
+    def set_user(self, user):
+        self.user = user
+    
+    def clean(self):
+        current_password = self.cleaned_data.get('current_password')
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+    
+        if not self.user.check_password(current_password):
+            raise forms.ValidationError({"current_password": "Incorrect current password" })
+        if password != confirm_password:
+            raise forms.ValidationError({'confirm_password': "Password confirmation failed" })
+
+        return self.cleaned_data
+
+
+class UserForm(FormControlMixin, forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'groups']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['groups'].widget.attrs.update({
+            'class': 'form-control select2'
+        })
+        self.fields['email'].required = True
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError("User with this email address already exists")
+        
+        return email
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,65 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+import subprocess
+
+from django.conf import settings as conf_settings
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash, get_user_model
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import resolve, reverse, reverse_lazy
+from django.utils.crypto import get_random_string
+from django.views.generic import View, TemplateView, FormView, ListView, CreateView, UpdateView, DeleteView
+
+from .forms import (
+    ChangePasswordForm, 
+    LoginForm, 
+)
+
+from .mixins import (
+    BaseMixin, 
+    AuditCreateMixin, 
+    AuditUpdateMixin, 
+    AuditDeleteMixin, 
+    CustomLoginRequiredMixin, 
+    GetDeleteMixin, 
+    GroupRequiredMixin,
+    NonDeletedListMixin, 
+    NonLoginRequiredMixin, 
+    NonSuperAdminRequiredMixin, 
+    SuperAdminRequiredMixin
+)
+
+
 
 # Create your views here.
 class DashboardView(TemplateView):
     template_name = "dashboard/layouts/home.html"
+
+
+# Login Logout Views
+class LoginPageView(NonLoginRequiredMixin, FormView):
+    form_class = LoginForm
+    template_name = "dashboard/auth/login.html"
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+
+        # Remember me
+        if self.request.POST.get('remember', None) == None:
+            self.request.session.set_expiry(0)
+
+        login(self.request, user)
+        store_audit(request= self.request, instance=self.request.user, action='LOGIN')
+
+        if 'next' in self.request.GET:
+            return redirect(self.request.GET.get('next'))
+        return redirect('dashboard:index')
+
+class LogoutView(CustomLoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        store_audit(request= self.request, instance=self.request.user, action='LOGOUT')
+        logout(request)
+        return redirect('dashboard:login')
