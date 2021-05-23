15f380f34bfebaefa770276961f76183532fd014
ShakyaAnkit <shakyaankitt001@gmail.com>
2021-05-23 11:06:24 +0545

    sample list and form

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,5 +7,8 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', DashboardView.as_view(), name='home'),
     path('accounts/login/', LoginPageView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_view(), name='logout')
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+
+    path('dashboard/sample/list', SampleListView.as_view(), name='sample-list'),
+    path('dashboard/sample/form', SampleFormView.as_view(), name='sample-form')
 ]
