5d603ad2cc7a428f05baae4a895ab35a4cb59e5a
pra5151 <spranish45@gmail.com>
2021-05-23 11:47:46 +0545

    Audit trail list view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 
-from .views import *
+from . import views
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='home'),
-    path('accounts/login/', LoginPageView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 
-    path('dashboard/sample/list', SampleListView.as_view(), name='sample-list'),
-    path('dashboard/sample/form', SampleFormView.as_view(), name='sample-form')
+    # audit-trail
+    path('audits', views.AuditTrailListView.as_view(), name='audittrail-list'),
+
+    path('', views.DashboardView.as_view(), name='home'),
+    path('accounts/login/', views.LoginPageView.as_view(), name='login'),
+    path('accounts/logout/',views.LogoutView.as_view(), name='logout'),
+
+    path('dashboard/sample/list', views.SampleListView.as_view(), name='sample-list'),
+    path('dashboard/sample/form', views.SampleFormView.as_view(), name='sample-form')
 ]
