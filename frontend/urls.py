3f42699e2193c7983de2ba9a6e41bdec2a5cf65d
ShakyaAnkit <shakyaankitt001@gmail.com>
2021-07-31 20:43:11 +0545

    login and register integration

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,4 +6,6 @@ app_name="frontend"
 
 urlpatterns = [
    path('', views.HomeView.as_view(), name='home'),
+   path('login/', views.LoginView.as_view(), name='login'),
+   path('register/', views.SignUpView.as_view(), name='register'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 # Home
 class HomeView(TemplateView):
     template_name = 'frontend/layouts/home.html'
+
+class LoginView(TemplateView):
+    template_name = 'frontend/layouts/login.html'
+
+class SignUpView(TemplateView):
+    template_name = 'frontend/layouts/register.html'
